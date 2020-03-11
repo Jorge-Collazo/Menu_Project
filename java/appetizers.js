2aad159930b66641a757e7d99554628b07ef4674
Jorge Collazo <jcollazo151@west-mec.org>
2020-03-11 14:34:51 -0700

    Implemented javascript

---
+++ java/appetizers.js
@@ -0,0 +1,128 @@
+"use-strict"
+
+function cB(){
+    document.getElementById("cost").innerHTML = "$0.00"
+    document.getElementById("name").innerHTML = "Combination Plate"
+    document.getElementById("desc").innerHTML = "Paper wrapped chicken (2), BBQ ribs (2), skewered beef (2), egg rolls (2), fried wonton (4), and fried shrimp (2)"
+    document.getElementById("img").innerHTML = "<img src='images/dish.png' alt='CombinationPlate' width='80%'>"
+}
+function eR(){
+    document.getElementById("cost").innerHTML = "$0.00"
+    document.getElementById("name").innerHTML = "Egg Rolls (4)"
+    document.getElementById("desc").innerHTML = "Crispy and filled with carrots and cabbage."
+    document.getElementById("img").innerHTML = "<img src='images/logo.png' alt='CombinationPlate' width='80%'>"
+}
+function Tr(){
+    document.getElementById("cost").innerHTML = "$0.00"
+    document.getElementById("name").innerHTML = "Tottie Rolls (4)"
+    document.getElementById("desc").innerHTML = ""
+    document.getElementById("img").innerHTML = "<img src='images/logo.png' alt='CombinationPlate' width='80%'>"
+}
+function sER(){
+    document.getElementById("cost").innerHTML = "$0.00"
+    document.getElementById("name").innerHTML = "Shanghai Egg Rolls (4)"
+    document.getElementById("desc").innerHTML = ""
+    document.getElementById("img").innerHTML = "<img src='images/logo.png' alt='CombinationPlate' width='80%'>"
+}
+function sR(){
+    document.getElementById("cost").innerHTML = "$0.00"
+    document.getElementById("name").innerHTML = "Spring Rolls (4)"
+    document.getElementById("desc").innerHTML = ""
+    document.getElementById("img").innerHTML = "<img src='images/logo.png' alt='CombinationPlate' width='80%'>"
+}
+function tPS(){
+    document.getElementById("cost").innerHTML = "$0.00"
+    document.getElementById("name").innerHTML = "Traditional (pork & shrimp)"
+    document.getElementById("desc").innerHTML = ""
+    document.getElementById("img").innerHTML = "<img src='images/logo.png' alt='CombinationPlate' width='80%'>"    
+}  
+function s(){
+    document.getElementById("cost").innerHTML = "$0.00"
+    document.getElementById("name").innerHTML = "Salmon"
+    document.getElementById("desc").innerHTML = ""
+    document.getElementById("img").innerHTML = "<img src='images/logo.png' alt='CombinationPlate' width='80%'>"    
+}
+function sT(){
+    document.getElementById("cost").innerHTML = "$0.00"
+    document.getElementById("name").innerHTML = "Shrimp Tempura"
+    document.getElementById("desc").innerHTML = ""
+    document.getElementById("img").innerHTML = "<img src='images/logo.png' alt='CombinationPlate' width='80%'>"
+}
+function sSC(){
+    document.getElementById("cost").innerHTML = "$0.00"
+    document.getElementById("name").innerHTML = "Soft Shell Crab"
+    document.getElementById("desc").innerHTML = ""
+    document.getElementById("img").innerHTML = "<img src='images/logo.png' alt='CombinationPlate' width='80%'>"    
+}
+function v(){
+    document.getElementById("cost").innerHTML = "$0.00"
+    document.getElementById("name").innerHTML = "Vegetarian"
+    document.getElementById("desc").innerHTML = ""
+    document.getElementById("img").innerHTML = "<img src='images/logo.png' alt='CombinationPlate' width='80%'>"
+}
+function sCh(){
+    document.getElementById("cost").innerHTML = "$0.00"
+    document.getElementById("name").innerHTML = "Sugarcane Chicken"
+    document.getElementById("desc").innerHTML = ""
+    document.getElementById("img").innerHTML = "<img src='images/logo.png' alt='CombinationPlate' width='80%'>"    
+}
+function bOS(){
+    document.getElementById("cost").innerHTML = "$0.00"
+    document.getElementById("name").innerHTML = "Beef on Skewers (4)"
+    document.getElementById("desc").innerHTML = ""
+    document.getElementById("img").innerHTML = "<img src='images/logo.png' alt='CombinationPlate' width='80%'>"
+}
+function cSS(){
+    document.getElementById("cost").innerHTML = "$0.00"
+    document.getElementById("name").innerHTML = "Chicken Satay Sticks (2)"
+    document.getElementById("desc").innerHTML = ""
+    document.getElementById("img").innerHTML = "<img src='images/logo.png' alt='CombinationPlate' width='80%'>"    
+}
+function sSCPS(){
+    document.getElementById("cost").innerHTML = "$0.00"
+    document.getElementById("name").innerHTML = "Soft Shell Crab with Ponzu Sauce"
+    document.getElementById("desc").innerHTML = ""
+    document.getElementById("img").innerHTML = "<img src='images/logo.png' alt='CombinationPlate' width='80%'>"
+}
+function e(){
+    document.getElementById("cost").innerHTML = "$0.00"
+    document.getElementById("name").innerHTML = "Edamame"
+    document.getElementById("desc").innerHTML = ""
+    document.getElementById("img").innerHTML = "<img src='images/logo.png' alt='CombinationPlate' width='80%'>"    
+}
+function sP(){
+    document.getElementById("cost").innerHTML = "$0.00"
+    document.getElementById("name").innerHTML = "Shrimp Puffs (6)"
+    document.getElementById("desc").innerHTML = ""
+    document.getElementById("img").innerHTML = "<img src='images/logo.png' alt='CombinationPlate' width='80%'>"
+}
+function bbq(){
+    document.getElementById("cost").innerHTML = "$0.00"
+    document.getElementById("name").innerHTML = "BBQ Pork Ribs (4)"
+    document.getElementById("desc").innerHTML = ""
+    document.getElementById("img").innerHTML = "<img src='images/logo.png' alt='CombinationPlate' width='80%'>"    
+}
+function fW(){
+    document.getElementById("cost").innerHTML = "$0.00"
+    document.getElementById("name").innerHTML = "Fried Wonton (10)"
+    document.getElementById("desc").innerHTML = ""
+    document.getElementById("img").innerHTML = "<img src='images/logo.png' alt='CombinationPlate' width='80%'>"
+}
+function fT(){
+    document.getElementById("cost").innerHTML = "$0.00"
+    document.getElementById("name").innerHTML = "Fried Tofu (8)"
+    document.getElementById("desc").innerHTML = ""
+    document.getElementById("img").innerHTML = "<img src='images/logo.png' alt='CombinationPlate' width='80%'>"    
+}
+function p(){
+    document.getElementById("cost").innerHTML = "$0.00"
+    document.getElementById("name").innerHTML = "Potstickers (5)"
+    document.getElementById("desc").innerHTML = ""
+    document.getElementById("img").innerHTML = "<img src='images/logo.png' alt='CombinationPlate' width='80%'>"
+}
+function sC(){
+    document.getElementById("cost").innerHTML = "$0.00"
+    document.getElementById("name").innerHTML = "Spicy Cucumber"
+    document.getElementById("desc").innerHTML = ""
+    document.getElementById("img").innerHTML = "<img src='images/logo.png' alt='CombinationPlate' width='80%'>"   
+}
